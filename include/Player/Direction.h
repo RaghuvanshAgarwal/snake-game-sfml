f4652708e0918b29e7ae761c51b740d5a569b632
Raghuvansh Agarwal <rarchva@gmail.com>
2025-10-29 10:59:42 +0530

    Snake Position and Direction

---
+++ include/Player/Direction.h
@@ -0,0 +1,16 @@
+//
+// Created by Raghuvansh Agarwal on 29/10/25.
+//
+
+#ifndef SNAKE_GAME_DIRECTION_H
+#define SNAKE_GAME_DIRECTION_H
+
+namespace Player {
+    enum class Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+    };
+}
+#endif //SNAKE_GAME_DIRECTION_H
--- include/Player/SnakeController.h
+++ include/Player/SnakeController.h
@@ -4,6 +4,8 @@
 
 #ifndef SNAKE_GAME_SNAKECONTROLLER_H
 #define SNAKE_GAME_SNAKECONTROLLER_H
+#include "Direction.h"
+#include "SFML/System/Vector2.hpp"
 
 namespace Player {
     enum class SnakeState {
@@ -12,7 +14,11 @@ namespace Player {
     };
     class SnakeController {
         const int initial_snake_length = 10;
+        const Direction default_direction = Direction::RIGHT;
+        const sf::Vector2u default_snake_position = sf::Vector2u(25, 13);
+
         SnakeState snake_state = SnakeState::ALIVE;
+        Direction current_snake_direction = default_direction;
 
         void processPlayerInput();
         void updateSnakeDirection();
