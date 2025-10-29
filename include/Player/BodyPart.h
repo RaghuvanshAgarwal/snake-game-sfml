17ae4971138d14a35936dfd9f1b1506b206b118a
Raghuvansh Agarwal <rarchva@gmail.com>
2025-10-29 14:22:49 +0530

    Getting ready to draw body part

--- include/Player/BodyPart.h
+++ include/Player/BodyPart.h
@@ -13,11 +13,22 @@ namespace UI::UIElement {
 
 namespace Player {
     class BodyPart {
-        UI::UIElement::ImageView* body_part_image = nullptr;
+        UI::UIElement::ImageView *body_part_image = nullptr;
         sf::Vector2i grid_position = sf::Vector2i(0, 0);
         Direction direction = Direction::RIGHT;
         float body_part_width = 0.f;
         float body_part_height = 0.f;
+
+        void createBodyPartImage();
+        sf::Vector2f getBodyPartScreenPosition();
+        void initializeBodyPartImage();
+
+    public:
+        BodyPart(); // Default constructor
+        ~BodyPart(); // Destructor
+        void initialize(float width,float height, sf::Vector2i pos, Direction dir);
+        void update();
+        void render() const; // To be called on every frame
     };
 } // Player
 
--- source/Player/BodyPart.cpp
+++ source/Player/BodyPart.cpp
@@ -4,5 +4,47 @@
 
 #include "../../include/Player/BodyPart.h"
 
+#include "../../include/Global/Config.h"
+#include "../../include/Global/ServiceLocator.h"
+#include "../../include/UI/UIElement/ImageView.h"
+
 namespace Player {
+    using namespace UI::UIElement;
+    void BodyPart::createBodyPartImage() {
+        body_part_image = new ImageView();
+    }
+
+    void BodyPart::initializeBodyPartImage() {
+        body_part_image->initialize(Global::Config::snake_body_texture_path, body_part_width,body_part_height,getBodyPartScreenPosition());
+        body_part_image->setCentreAlinged();
+    }
+
+    BodyPart::BodyPart() {
+        grid_position = sf::Vector2i(0, 0);
+        createBodyPartImage();
+    }
+
+    BodyPart::~BodyPart() {
+        delete body_part_image;
+    }
+
+    void BodyPart::initialize(const float width, const float height, sf::Vector2i pos, Direction dir) {
+        direction = dir;
+        grid_position = pos;
+        body_part_width = width;
+        body_part_height = height;
+        initializeBodyPartImage();
+    }
+
+    void BodyPart::update() {
+
+    }
+
+    void BodyPart::render() const {
+        body_part_image->render();
+    }
+
+    sf::Vector2f BodyPart::getBodyPartScreenPosition() {
+        Global::ServiceLocator::getInstance()->getLevelService()->
+    }
 } // Player
